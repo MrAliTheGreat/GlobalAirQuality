675874ad5ac9309aa0c29419e954e9de5cb0a721
Ali Bahari <abahari@terrence2.cs.mun.ca>
2024-10-18 17:28:49 -0230

    Add city name to AQI merged csv

--- utils/mergeTabular.py
+++ utils/mergeTabular.py
@@ -26,6 +26,7 @@ else:
 
 for file in os.listdir(srcDir):
     df = pd.read_csv(f"{srcDir}/{file}")
+    df.Filename = file[:-4] + "/" + df.Filename
     df = df.sample(n = sampleCount)
     if(not os.path.exists(mergedPath)):
         df.to_csv(mergedPath, index = False, header = True)
