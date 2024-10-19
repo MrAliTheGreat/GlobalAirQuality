86283813d25cd76fbd8cb658c8682452dedd2947
Ali Bahari <abahari@terrence2.cs.mun.ca>
2024-10-19 12:58:46 -0230

    Add params error handling for pngCount

--- utils/pngCount.sh
+++ utils/pngCount.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
-find $2 -type f -name "*.png" | cut -d "/" -f 3 | uniq -c | sort -n
+if [[ $# == 2 && $1 == "-s" ]]; then
+    find $2 -type f -name "*.png" | cut -d "/" -f 3 | uniq -c | sort -n
+else
+    echo "Wrong format! Use -s flag to determine source directory of png files"
+    echo "Example: ./pngCount.sh -s ./dataset/"
+fi
